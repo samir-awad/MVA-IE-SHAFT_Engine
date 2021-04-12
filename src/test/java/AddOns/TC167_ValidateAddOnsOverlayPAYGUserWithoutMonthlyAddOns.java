5ffc676bc6e857220395768482d921ec2404f8a6
NoraAhmedVodafone <80685697+NoraAhmedVodafone@users.noreply.github.com>
2021-04-12 19:22:08 +0200

    Add ons updates

--- src/test/java/AddOns/TC167_ValidateAddOnsOverlayPAYGUserWithoutMonthlyAddOns.java
+++ src/test/java/AddOns/TC167_ValidateAddOnsOverlayPAYGUserWithoutMonthlyAddOns.java
@@ -1,4 +1,45 @@
 package AddOns;
 
+import Pages.AddOns;
+import Pages.Home;
+import Pages.Login;
+import com.shaft.gui.browser.BrowserFactory;
+import com.shaft.validation.Assertions;
+import com.shaft.validation.Verifications;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class TC167_ValidateAddOnsOverlayPAYGUserWithoutMonthlyAddOns {
+    private WebDriver driver;
+    private Login LoginPage;
+    private Home HomePage;
+    private AddOns AddOnsPage;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = BrowserFactory.getBrowser();
+        LoginPage = new Login(driver);
+        HomePage = new Home(driver);
+        AddOnsPage=new AddOns(driver);
+        LoginPage.acceptTermsAndConditions().login().acceptPermissions();//fluent design
+    }
+
+    @Test
+    public void CheckEssentialsSection() {
+        Assertions.assertTrue(HomePage.checkEssentialsSection());
+    }
+
+    @Test(dependsOnMethods = {"CheckEssentialsSection"})
+    public void ValidateAddOnsOverlayText() {
+        HomePage.opedAddOnsOverlay();
+        Verifications.verifyTrue(AddOnsPage.checkAddOnsOverlay());
+        Assertions.assertElementAttribute(driver,AddOnsPage.getNoActiveAddOnsOverlayTxt(),"text","No active Add Ons");
+    }
+
+    @Test(dependsOnMethods = {"CheckEssentialsSection","ValidateAddOnsOverlay"})
+    public void ValidateAddOnsOverlayCloseButton(){
+        AddOnsPage.closeAddOnsOverlayBtn();
+        Assertions.assertElementAttribute(driver,HomePage.getEssentials_text(),"text","Essentials");
+    }
 }
