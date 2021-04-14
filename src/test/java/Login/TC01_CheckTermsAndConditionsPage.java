fdc3ca22dcb68c393a88d8515b4bfd995125af20
samir-awad <72711276+samir-awad@users.noreply.github.com>
2021-04-14 09:59:43 +0200

    Login Updated

---
+++ src/test/java/Login/TC01_CheckTermsAndConditionsPage.java
@@ -0,0 +1,48 @@
+package Login;
+
+import Pages.Login;
+import com.shaft.gui.browser.BrowserFactory;
+import com.shaft.validation.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+
+public class TC01_CheckTermsAndConditionsPage{
+	
+	//TC001 - Check Terms & Conditions Page Android
+    //TC013 - Check Terms & Conditions page iOS
+
+    private WebDriver driver;
+    private Login LoginPage;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = BrowserFactory.getBrowser();
+        LoginPage = new Login(driver);
+    }
+
+
+
+    @Test()
+    public void Check_Terms_And_Conditions_Header_Is_Displayed(){
+        Assertions.assertElementExists(driver,LoginPage.getTermsAndConditions_Header());
+    }
+
+    @Test()
+    public void Check_Terms_And_Conditions_Text_Is_Displayed(){
+        Assertions.assertElementExists(driver,LoginPage.getTermsAndConditions_Txt());
+    }
+
+    @Test()
+    public void Check_Terms_And_Conditions_CheckBox_Is_Displayed(){
+        Assertions.assertElementExists(driver,LoginPage.getAcceptTerms_button());
+    }
+
+    @Test
+    public void Check_Terms_And_Conditions_Continue_Button_Is_Displayed(){
+        Assertions.assertElementExists(driver,LoginPage.getContinue_button());
+    }
+
+}
