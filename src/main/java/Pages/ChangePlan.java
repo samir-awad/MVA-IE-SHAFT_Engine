2bbe306fd083c513b0c753a7ac2b00c1429ce4bd
Rana Hamdy <Rana.hamdy2@vodafone.com>
2021-04-14 17:18:31 +0200

    New files

--- src/main/java/Pages/ChangePlan.java
+++ src/main/java/Pages/ChangePlan.java
@@ -6,31 +6,45 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class ChangePlan {
-    public ChangePlan(WebDriver driver) {
-        this.driver = driver;
-    }
 
     private WebDriver driver;
-    private By PlanTitle = By.xpath("//android.widget.TextView[@content-desc=\"id_dashboard_your_plan_details_title\"]");
-    private By ChangePlan_Button = By.xpath("//android.widget.Button[@content-desc=\"id_dashboard_your_plan_details_button\"]/android.widget.TextView");
-    private By YourPlanClose_Button = By.xpath("//android.widget.ImageView[@content-desc=\"id_dashboard_your_plan_details_close\"]");
-
-    ////
-    private By ChangePlanTitle = new MobileBy.ByAccessibilityId("id_header_title_Change_plan");
-    private By ChangePlanClose_Button = new MobileBy.ByAccessibilityId("id_header_close_icon\"");
-    private By ViewCurrentPlanDetails = new MobileBy.ByAccessibilityId("id_current_plan_view_details");
-    private By SelectPlan_Button = new MobileBy.ByAccessibilityId("id_change_plan_Bill Pay_select_button");
-    private By ViewDetails_Button = new MobileBy.ByAccessibilityId("id_change_plan_Bill Pay_view_button");
-    private By SimOnlyTab = new MobileBy.ByAccessibilityId("SIM only");
-    private By CloseCurrentPlanDetails = By.xpath("//android.widget.ImageView[@content-desc=\"id_change_plan_close\"]");
-    ////
-
-    private By AvailablePlanDetails = new MobileBy.ByAccessibilityId("");
-    private By PlanDetailsClose_Button = new MobileBy.ByAccessibilityId("");
-    private By PlanDetailsTitle = By.xpath("(//android.widget.TextView[@content-desc=\"id_change_plan_title\"])[1]");
+    private By PlanTitle ;
+    private By ChangePlan_Button ;
+    private By YourPlanClose_Button;
+    private By ChangePlanTitle;
+    private By ChangePlanClose_Button;
+    private By ViewCurrentPlanDetails;
+    private By SelectPlan_Button ;
+    private By ViewDetails_Button ;
+    private By SimOnlyTab;
+    private By CloseCurrentPlanDetails;
+    private By AvailablePlanDetails;
+    private By PlanDetailsClose_Button;
+    private By PlanDetailsTitle;
 
 
-    public Boolean yourPlanTitle() {
+    public ChangePlan(WebDriver driver) {
+        this.driver = driver;
+        if (System.getProperty("targetOperatingSystem").equals("Android")){
+         PlanTitle = By.xpath("//android.widget.TextView[@content-desc=\"id_dashboard_your_plan_details_title\"]");
+         ChangePlan_Button = By.xpath("//android.widget.Button[@content-desc=\"id_dashboard_your_plan_details_button\"]/android.widget.TextView");
+         YourPlanClose_Button = By.xpath("//android.widget.ImageView[@content-desc=\"id_dashboard_your_plan_details_close\"]");
+         ChangePlanTitle = new MobileBy.ByAccessibilityId("id_header_title_Change_plan");
+         ChangePlanClose_Button = new MobileBy.ByAccessibilityId("id_header_close_icon\"");
+         ViewCurrentPlanDetails = new MobileBy.ByAccessibilityId("id_current_plan_view_details");
+         SelectPlan_Button = new MobileBy.ByAccessibilityId("id_change_plan_Bill Pay_select_button");
+         ViewDetails_Button = new MobileBy.ByAccessibilityId("id_change_plan_Bill Pay_view_button");
+         SimOnlyTab = new MobileBy.ByAccessibilityId("SIM only");
+         CloseCurrentPlanDetails = By.xpath("//android.widget.ImageView[@content-desc=\"id_change_plan_close\"]");
+         AvailablePlanDetails = new MobileBy.ByAccessibilityId("");
+         PlanDetailsClose_Button = new MobileBy.ByAccessibilityId("");
+         PlanDetailsTitle = By.xpath("(//android.widget.TextView[@content-desc=\"id_change_plan_title\"])[1]");
+
+        }else{
+        }
+    }
+
+    public Boolean checkYourPlanOverlay() {
         return ElementActions.isElementDisplayed(driver, PlanTitle);
     }
 
@@ -38,15 +52,15 @@ public class ChangePlan {
         ElementActions.performTouchAction(driver).tap(YourPlanClose_Button);
     }
 
-    public void pressSelectChangePlanButton() {
+    public void pressYourPlanOverlayChangePlanButton() {
         ElementActions.performTouchAction(driver).tap(ChangePlan_Button);
     }
 
-    public Boolean ChangePlanTitle() {
+    public Boolean checkChangePlanPageHeader() {
         return ElementActions.isElementDisplayed(driver, ChangePlanTitle);
     }
 
-    public void checkCurrentPlanDetails() {
+    public void pressViewCurrentPlanDetails() {
         ElementActions.performTouchAction(driver).tap(ViewCurrentPlanDetails);
     }
 
@@ -58,7 +72,7 @@ public class ChangePlan {
         ElementActions.performTouchAction(driver).tap(SimOnlyTab);
     }
 
-    public void changePlanCloseBTN() {
+    public void pressCloseButton() {
         ElementActions.performTouchAction(driver).tap(ChangePlanClose_Button);
     }
 
@@ -69,10 +83,10 @@ public class ChangePlan {
     public void checkAvailablePlanDetailsOverlay() {
         ElementActions.performTouchAction(driver).tap(AvailablePlanDetails);
     }
-    public void pressPlanDetailsOverlayCloseBtn(){
+    public void pressPlanDetailsOverlayCloseButton_2(){
         ElementActions.performTouchAction(driver).tap(PlanDetailsClose_Button);
     }
-    public void pressViewDetailsButton(){
+    public void pressViewSelectedPlansDetails(){
         ElementActions.performTouchAction(driver).tap(ViewDetails_Button);
     }
     public Boolean planDetailsTitle(){
